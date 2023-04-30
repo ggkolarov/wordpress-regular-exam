8d87defa796c158ee819f18822f9de6e1ad9ae69
ggkolarov <georgikollarov@gmail.com>
2023-04-30 16:53:04 +0300

    Added ACF for modifying custom post type;

--- wp-content/themes/theme-homes/template-parts/home-item.php
+++ wp-content/themes/theme-homes/template-parts/home-item.php
@@ -2,11 +2,11 @@
     <div class="property-primary">
         <h2 class="property-title"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
         <div class="property-meta">
-            <span class="meta-location">Ovcha Kupel, Sofia</span>
-            <span class="meta-total-area">Total area: 91.65 sq.m</span>
+            <span class="meta-location"><?php the_field( 'property_location' ) ?></span>
+            <span class="meta-total-area">Total area: <?php the_field( 'property_total_area' ) ?></span>
         </div>
         <div class="property-details">
-            <span class="property-price">€ 100,815</span>
+            <span class="property-price">€ <?php the_field( 'property_price' ) ?></span>
             <span class="property-date">Posted <?php the_date(); ?> days ago</span>
         </div>
     </div>
